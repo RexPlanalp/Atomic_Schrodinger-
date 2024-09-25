114002d66d259bee6b0eb24b1c692e9dec7114e8
PLANALP Donald Rex <dopl4670@terra.colorado.edu>
2024-09-25 12:56:48 -0600

    Pycache for all other than research plots, which added a test file for messin with asymmetry and messing with channel plot

--- Research_Plotting/Channel_Plot.py
+++ Research_Plotting/Channel_Plot.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 # Ip = -0.5791546178 # Ionization potential of species in atomic units
 Ip = -0.5
 N = 10 # Numbe08r of cycles of laser pulse 
-w = 0.057 # Central Frequency of laser pulse in atomic units
+w = 0.057# Central Frequency of laser pulse in atomic units
 I = 2e14 / 3.51E16 # Intensity of laser pulse in atomic units
-E = 0.54 # Energy in atomic units (usually at ATI peak)
+E = 0.48 # Energy in atomic units (usually at ATI peak)
 #E = 0.608
 
 tau = 2*np.pi/w # Period of laser pulse in atomic units
