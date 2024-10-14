242c4f106b36e027b45bf8093a1c8495ac5e1035
PLANALP Donald Rex <dopl4670@terra.colorado.edu>
2024-10-14 14:29:02 -0600

    changed laser_info.py

--- General_Plotting/Laser_Info.py
+++ General_Plotting/Laser_Info.py
@@ -5,22 +5,21 @@ import matplotlib.pyplot as plt
 
 Ip = 0.5
 N = 10
-wavelength = 800
+wavelength = 400
 w = 0.057 * 800/wavelength
 I = 2e14 / 3.51E16
 
 tau = 2*np.pi/w
-t = np.linspace(0,N*tau,1000)
+t = np.linspace(0,tau,1000)
 envelope = np.sin(np.pi*t/tau)**2
 I_profile = I * envelope
 Up = I_profile/(4*w**2)
 
+E = 0.195
+
 def print_direct_cutoff():
-    energy = Ip + np.max(Up)
+    energy = Ip + 2*np.max(Up)
     print(f"Direct cutoff energy is {energy}")
-def print_rescattering_cutoff():
-    energy = Ip + 10 * np.max(Up)
-    print(f"Rescattering cutoff energy is {energy}")
 def print_keldysh_parameter():
     gamma = np.sqrt(Ip/(2*np.max(Up)))
     print(f"Keldysh parameter is {gamma}")
@@ -47,9 +46,11 @@ def plot_channels(n):
             
 if __name__ == "__main__":
     print_direct_cutoff()
-    print_rescattering_cutoff()
+    print("\n")
     print_keldysh_parameter()
+    print("\n")
     print_intensity_parameter()
-    n = find_photons(1.4)
+    print("\n")
+    n = find_photons(E)
     plot_channels(n)
     print("Done")
